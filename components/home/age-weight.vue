2d4152fc2f82ba5e1b81ed860f6e7d46d486d205
Pawitra Warda <pawitrawarda@gmail.com>
2020-04-29 15:48:07 +0700

    Initial commit

---
+++ components/home/age-weight.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "age-weight"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/home/gender.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "gender"
+    }
+</script>
+
+<style scoped>
+
+</style>
